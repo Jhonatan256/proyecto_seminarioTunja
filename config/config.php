8477c04c9d93b78020a27141604f41eb9df8b416
Jhonatan Andrés <jhot.256@gmail.com>
2023-03-09 20:54:48 -0500

    DEBUG

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ define('TIPO_AMBIENTE', 'pruebas');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_TABLE', 'bd_seminario');
+define('DB_TABLE', 'bd_seminario2');
 define('DB_PORT', '3306');
 
 define('SERVER_EMAIL_HOST', 'smtp.gmail.com');
